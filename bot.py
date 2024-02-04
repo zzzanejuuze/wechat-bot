d5afca687b93fc7a81e6ec91c9f2181ee7b428be
Juze Zeng <zengjz03@vanke.com>
2024-02-04 22:47:47 +0800

    adding function calling

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from lib import itchat
 from lib.itchat.content import TEXT
-from gpt import chat, generate_img
+from gpt import chat, generate_img, chat_test
 from bot_memory import Bot_memory
 
 
@@ -20,28 +20,28 @@ def reply_msg(msg):
     else:
         user_msg = {"role": "user", "content": message}
         Bot_memory().save_memory(user_msg)
-        message_record = Bot_memory.memory_list
+        messages = Bot_memory.memory_list
         try:
-            bot_res = chat(message_record, maxtokens=4000)
-            print(message)
-            print(bot_res)
+            bot_res = chat_test(messages=messages, maxtokens=3800)
             bot_msg = {"role": "system", "content": bot_res}
+            print(bot_msg, 'hihi2')
             Bot_memory().save_memory(bot_msg)
-            print(Bot_memory.memory_list)
             itchat.send_msg(msg=bot_res, toUserName=msg['FromUserName'])
+
         except Exception as e:
             print(e)
             Bot_memory().clear_memory()
 
             user_msg = {"role": "user", "content": message}
             Bot_memory().save_memory(user_msg)
-            message_record = Bot_memory.memory_list
-            bot_res = chat(message_record, maxtokens=4000)
+            messages = Bot_memory.memory_list
+            bot_res = chat_test(messages=messages, maxtokens=3800)
 
             bot_msg = {"role": "system", "content": bot_res}
             Bot_memory().save_memory(bot_msg)
-            print(Bot_memory.memory_list)
+            #print(Bot_memory.memory_list)
             itchat.send_msg(msg=bot_res, toUserName=msg['FromUserName'])
+        print(Bot_memory.memory_list, 'hihi1')
 
 
 # for group chat
